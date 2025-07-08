13f7339f1382b9243ad302cee34c4c40c61e221b
AdepuAdithya <adithya.adepu@gmail.com>
2025-07-08 22:18:47 +0530

    updated

--- dataflows/gcs_to_raw/src/jlk.py
+++
@@ -1,5 +0,0 @@
-from apache_beam.io.gcp.gcsfilesystem import GCSFileSystem
-
-pipeline_options = PipelineOptions(project="your-project-id")
-gcs_fs = GCSFileSystem(pipeline_options)
-print(gcs_fs.exists("gs://your-bucket-name/path-to-your-csv-files/sample.csv"))
